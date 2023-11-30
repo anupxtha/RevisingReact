a59f36cc68d397aa2fb1a1eac946768c77ab9a6f
anupxtha <anupxtha2056@gmail.com>
2023-11-30 18:37:11 +0545

    Basic JSX rules and Nested Components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Greeting() {
+  return (
+    <div>
+      <Title />
+      <Message />
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Title = () => {
+  return <h1>This is Anup and this is my first component</h1>;
+};
+
+const Message = () => {
+  return <p>Hey Hey Hey</p>;
+};
+
+// // It is simpler to up functional Component Greeting // //
+// const Greeting = () => {
+//   return React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h1', {}, 'Hello World')
+//   );
+// };
+
+ReactDOM.render(<Greeting />, document.getElementById('root'));
