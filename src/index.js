4566f46c3f4747dc8daeed1df8704f6e251efcc8
anupxtha <anupxtha2056@gmail.com>
2023-12-05 14:09:13 +0545

    Booking project using jsx javascript and multiple component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 function Greeting() {
   return (
-    <div>
-      <Title />
-      <Message />
-    </div>
+    <section className='bookList'>
+      <Book />
+    </section>
   );
 }
 
-const Title = () => {
-  return <h1>This is Anup and this is my first component</h1>;
+const Book = () => {
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
 };
 
-const Message = () => {
-  return <p>Hey Hey Hey</p>;
+const Image = () => (
+  <img
+    src='https://images-na.ssl-images-amazon.com/images/I/81tdvyI0MeL._AC_UL254_SR254,254_.jpg'
+    alt=''
+  />
+);
+
+const title = 'Friends and Lovers';
+const Title = () => {
+  return <h1>{title}</h1>;
+};
+
+const Author = () => {
+  const author = 'Chandler';
+  return (
+    <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
+      {author.toUpperCase}
+    </h4>
+  );
 };
 
 // // It is simpler to up functional Component Greeting // //
