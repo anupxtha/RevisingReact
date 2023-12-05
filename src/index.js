3935514bc6dea2a91ef85b9fde340066aa77b1f3
anupxtha <anupxtha2056@gmail.com>
2023-12-05 14:49:31 +0545

    Props with variables and Objects

--- src/index.js
+++ src/index.js
@@ -2,42 +2,71 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//Variables
+const img =
+  'https://images-na.ssl-images-amazon.com/images/I/81tdvyI0MeL._AC_UL254_SR254,254_.jpg';
+
+const title = 'The Book of Friends';
+const author = 'Chandler';
+
+//Objects
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/51i6KX4JNIL._AC_SX184_.jpg',
+  title: 'White Bird: A Novel',
+  author: 'R. J. Palacio, Erica S. Perl',
+};
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/51RYHlz+7OL._AC_SX184_.jpg',
+  title: 'White Bird: A Novel',
+  author: 'R. J. Palacio, Erica S. Perl',
+};
+
 function Greeting() {
   return (
     <section className='bookList'>
-      <Book />
+      <Book bookImg={img} bookTitle={title} bookAuthor={author} />
+      <Book book={firstBook} />
+      <Book book={secondBook} />
     </section>
   );
 }
 
-const Book = () => {
+const Book = props => {
+  console.log(props);
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      {props.book ? (
+        <>
+          {/* Props for Objects */}
+          <img src={props.book.img} alt='' />
+          <h1>{props.book.title}</h1>
+          <h4
+            style={{
+              color: '#617d98',
+              fontSize: '0.75rem',
+              marginTop: '0.25rem',
+            }}
+          >
+            {props.book.author}
+          </h4>
+        </>
+      ) : (
+        <>
+          {/* Props for Variables */}
+          <img src={props.bookImg} alt='' />
+          <h1>{props.bookTitle}</h1>
+          <h4
+            style={{
+              color: '#617d98',
+              fontSize: '0.75rem',
+              marginTop: '0.25rem',
+            }}
+          >
+            {props.bookAuthor}
+          </h4>
+        </>
+      )}
     </article>
-  );
-};
-
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/81tdvyI0MeL._AC_UL254_SR254,254_.jpg'
-    alt=''
-  />
-);
-
-const title = 'Friends and Lovers';
-const Title = () => {
-  return <h1>{title}</h1>;
-};
-
-const Author = () => {
-  const author = 'Chandler';
-  return (
-    <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
-      {author.toUpperCase}
-    </h4>
   );
 };
 
